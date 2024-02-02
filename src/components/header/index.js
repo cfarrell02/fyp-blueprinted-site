960447c8d72272a527aa5da0290c6aa132255140
Cian Farrell <cianfarrell02@gmail.com>
2024-02-02 19:45:29 +0000

    Tweaked colours

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,9 +9,15 @@ const Header = () => {
         palette: {
             primary: {
                 // Light Blue
-                main: '#03a9f4',
+                main: '#418AB3',
                 // White text
                 contrastText: '#ffffff',
+            },
+            secondary: {
+                // Emergency Yellow
+                main: '#ffbf00',
+                // Black text
+                contrastText: '#000000', 
             },
         },
     });
@@ -24,12 +30,12 @@ const Header = () => {
   <AppBar position="static" sx={{ padding: '1em 3em', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
     <Typography variant="h4" sx={{ margin: '', color: 'white' }}>Blueprinted</Typography>
     <div sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-      <Link href="/fyp-blueprinted-site" sx={{ margin: '0 1em 0 0', color: 'white' }}>Home</Link>
-      <Link href="/fyp-blueprinted-site/poster" sx={{ margin: '0 1em 0 0', color: 'white' }}>Poster</Link>
+      <Link to="/fyp-blueprinted-site" sx={{ margin: '0 1em 0 0', color: 'white' }}>Home</Link>
+      <Link to="/fyp-blueprinted-site/poster" sx={{ margin: '0 1em 0 0', color: 'white' }}>Poster</Link>
     </div>
   </AppBar>
   {underConstruction && <>
-  <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', bgcolor: 'yellow', width: '100%', padding: '0 0 1em 0' }}>
+  <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', bgcolor: 'secondary.main', width: '100%', padding: '0 0 1em 0' }}>
     <WarningAmberIcon sx={{ margin: '.5em 1em 0' }} />
     <Typography variant="h6" sx={{ margin: '1em 0 0 0' }}>Website is under construction</Typography>
     <WarningAmberIcon sx={{ margin: '.5em 1em 0' }} />
